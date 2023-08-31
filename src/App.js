1c1184c2e75d7bd0b686a1fa90e9c12fbca58f51
Zaid <zedpx101@outlook.com>
2023-08-31 11:59:45 +0100

    buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import { evaluate } from 'mathjs';
 
 const App = () => {
 
-  const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "x", "/", "="];
+  const[numb, setNumb] = useState("");
+
+  const buttons = ["7","8","9","C","4","5","6", "x", "1", "2", "3", "/", "0", "+", "-", "="];
 
   const handleCLick = buttons.map((val) => {
     return val
@@ -11,7 +14,8 @@ const App = () => {
   
   return (
     <div className="App">
-      
+      <h1>{numb}</h1>
+        <button></button>
     </div>
   );
 };
