2b9b74584b8974b9c623b5e5008953d15dbc120a
Zaid <zedpx101@outlook.com>
2023-08-31 11:36:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { evaluate } from 'mathjs';
 
-function App() {
+const App = () => {
+
+  const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "x", "/", "="];
+
+  const handleCLick = buttons.map((val) => {
+    return val
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
-}
-
+};
 export default App;
